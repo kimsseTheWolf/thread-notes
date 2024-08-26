49a6f54de92913a771dcf4df7616b95ad6c8fc14
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2024-08-26 10:30:16 -0400

    Updating Image viewer for easier image viewing

--- src/renderer/src/components/MessageBubble.vue
+++ src/renderer/src/components/MessageBubble.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { Flex, Avatar, Menu, MenuItem, Dropdown, Button } from 'ant-design-vue';
+import { EllipsisOutlined } from '@ant-design/icons-vue';
 import { UserOutlined } from '@ant-design/icons-vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import TextEditor from './TextEditor.vue';
+import Viewer from 'viewerjs';
+import 'viewerjs/dist/viewer.css'
 
 const isInEdit = ref(false)
 
@@ -23,6 +26,14 @@ function handleEditSave() {
     emits("onUpdateContent", props.uuid, message.value)
 }
 
+onMounted(() => {
+    const imgViewer = new Viewer(document.getElementById('message-content'), {
+        toolbar: false,
+        navbar: false,
+        title: false,
+    })
+})
+
 </script>
 <template>
     <Dropdown :trigger="['contextmenu']">
@@ -41,13 +52,25 @@ function handleEditSave() {
                         </template>
                     </Avatar>
                     <b>You</b>
+                    <div></div>
                 </Flex>
                 <Flex gap="small" justify="end" v-if="!isInEdit">
-                    <Button @click="isInEdit = true" size="small">Edit</Button>
+                    <Dropdown>
+                        <template #overlay>
+                            <Menu>
+                                <MenuItem key="0" @click="isInEdit = true" >Edit</MenuItem>
+                                <MenuItem key="1">Move Up</MenuItem>
+                                <MenuItem key="2">Move Down</MenuItem>
+                            </Menu>
+                        </template>
+                        <Button size="small">
+                            <EllipsisOutlined/>
+                        </Button>
+                    </Dropdown>
                     <Button size="small" @click="emits('onDelete', props.uuid)">Delete</Button>
                 </Flex>
             </Flex>
-            <div v-html="message" v-if="!isInEdit"></div>
+            <div v-html="message" v-if="!isInEdit" id="message-content"></div>
             <Flex gap="small" v-if="isInEdit">
                 <TextEditor v-model="message"/>
                 <Button type="primary" @click="handleEditSave">Save</Button>
